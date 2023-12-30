2d792d66e8d8dd25a9cbf45aebd0a9f0e8052cd7
Rotich <jane.rotich@student.moringaschool.com>
2023-12-30 15:46:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+// set up
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has loaded");
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Target the paragraph element with id="text" ie <p id = "text"
+    const textOfParagraph = document.getElementById('text');
+  
+    // Replacing the text content with "This is really cool!"
+    if (textOfParagraph) {
+        textOfParagraph.textContent = "This is really cool!";
+    }
+  });
+  
